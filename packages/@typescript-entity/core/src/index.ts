31fc00a82d45f56d146fc5a029505a456aa1be9b
Adam Pancutt <adam@pancutt.com>
2020-10-17 09:49:21 +0200

    Export RethrownError

--- packages/@typescript-entity/core/src/index.ts
+++ packages/@typescript-entity/core/src/index.ts
@@ -3,5 +3,6 @@ export * from "./AttrValueError";
 export * from "./Entity";
 export * from "./EntityError";
 export * from "./InvalidAttrValueError";
+export * from "./RethrownError";
 export * from "./UnknownAttrError";
 export * from "./UnsanitizableAttrError";
